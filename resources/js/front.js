7c2e0bdd7755e3283be2f6df1191e24b49822b0e
bittoMazza <robertomazza99@gmail.com>
2022-09-29 14:07:18 +0200

    complete router js and import router in front.js

--- resources/js/front.js
+++ resources/js/front.js
@@ -1,8 +1,15 @@
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+
 window.Vue = require('vue');
 
+Vue.use(VueRouter)
+
 import App from './views/App';
+import router from './router';
 
 const app = new Vue({
     el: '#root',
-    render: h => h(App)
+    render: h => h(App),
+    router
 });
--- resources/js/router.js
+++ resources/js/router.js
@@ -3,3 +3,35 @@ import VueRouter from "vue-router";
 
 Vue.use(VueRouter)
 
+// Importiamo le rotte alle nostre pagine
+
+import AboutPage from './pages/AboutPage';
+import ContactsPage from './pages/ContactsPage';
+import HomePage from './pages/HomePage';
+import SinglePost from './pages/SinglePost';
+
+const router = new VueRouter({
+    mode:'history',
+    routes:[
+        {
+            path: '/',
+            name: 'home',
+            component: HomePage
+        },
+        {
+            path: '/contacts',
+            name: 'contacts',
+            component: ContactsPage
+        }, {
+            path: '/about',
+            name: 'about',
+            component: AboutPage
+        }, 
+       /*  {
+            path: '/',
+            name: 'home',
+            component: HomePage
+        }, */
+    ]
+})
+export default router
